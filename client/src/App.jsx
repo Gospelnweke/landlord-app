cec05b4d6dd13159ffa4641f4083270985d3e9ab
Gospel Nweke <nwekeug4307@gmail.com>
2025-03-09 10:16:16 +0000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+
+import './App.css';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="text-blue-800">
+      <p>Hello world!</p>
+    </div>
+  );
+};
+
+export default App;
