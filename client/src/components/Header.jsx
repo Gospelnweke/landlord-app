5e05a8088c2d340ecd4d384e188d9ae5f80389d2
Gospel Nweke <nwekeug4307@gmail.com>
2025-03-21 12:41:03 +0000

    db connected

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -59,13 +59,20 @@ const Header = () => {
           </button>
         </div>
         <ul className={`flex gap-4 ${isMenuOpen ? 'block' : 'hidden'} sm:flex`}>
-          <li className="hover:cursor-pointer hover:underline text-slate-700">
+          <li className="relative mr-10 kik hover:cursor-pointer hover:underline text-slate-700 group">
             Home
+            <div className="absolute left-0 mt-2 w-48 bg-white border border-gray-200 rounded-md shadow-lg hidden group-hover:block">
+              <ul>
+                <li className="px-4 py-2 hover:bg-gray-100">Submenu 1</li>
+                <li className="px-4 py-2 hover:bg-gray-100">Submenu 2</li>
+                <li className="px-4 py-2 hover:bg-gray-100">Submenu 3</li>
+              </ul>
+            </div>
           </li>
-          <li className="relative hover:cursor-pointer hover:underline text-slate-700">
+          <li className="mr-10 relative hover:cursor-pointer hover:underline text-slate-700">
             <button
               onClick={toggleDropdown}
-              className="focus:outline-none hover:cursor-pointer hover:underline"
+              className="kik focus:outline-none hover:cursor-pointer hover:underline"
             >
               About
             </button>
@@ -82,8 +89,22 @@ const Header = () => {
               </div>
             )}
           </li>
-          <li className="hover:cursor-pointer hover:underline text-slate-700">
+          <li className="kik flex items-center hover:cursor-pointer hover:underline text-slate-700">
             Sign in
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
           </li>
         </ul>
       </div>
